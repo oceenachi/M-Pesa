a5b423290af3b9287691850cb3618c282d10b5c2
oceenachi <phillipfrancisca@gmail.com>
2020-03-08 15:45:51 +0100

    create loan entity

--- src/main/java/com/oaf/demo/model/Loan.java
+++ src/main/java/com/oaf/demo/model/Loan.java
@@ -1,22 +1,26 @@
 package com.oaf.demo.model;
 
+
 import lombok.Data;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigDecimal;
+import javax.validation.constraints.NotBlank;
 
 
 @Data
 @Entity
-public class BankDetails {
+public class Loan {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String accountNumber;
+    private Long interestRate;
+
+    @NotBlank
+    private Long loanAmount;
 
 }
