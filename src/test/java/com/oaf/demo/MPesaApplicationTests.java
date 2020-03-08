7fa2a513302b1a7deeb95eb6131a0b24e4e15a6c
oceenachi <phillipfrancisca@gmail.com>
2020-03-08 14:35:14 +0100

    initial commit

---
+++ src/test/java/com/oaf/demo/MPesaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oaf.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MPesaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
