808e6390698fc6bc6e99e39d2bda801e98350e6a
oceenachi <phillipfrancisca@gmail.com>
2020-03-09 19:57:39 +0100

    modify loan and fund interfaces

--- src/main/java/com/oaf/demo/service/LoanInterface.java
+++ src/main/java/com/oaf/demo/service/LoanInterface.java
@@ -1,13 +1,22 @@
 package com.oaf.demo.service;
 
 
+import com.oaf.demo.model.Loan;
 import org.springframework.http.ResponseEntity;
 
 public interface LoanInterface {
 
-    ResponseEntity<?> getTotalLoanAmount();
+    Long getTotalLoanAmount(String id);
 
-    ResponseEntity<?> getLoanBreakdown();
+    ResponseEntity<?> getLoanBreakdown(String id, int span);
 
-    ResponseEntity<?> payLoan();
+    ResponseEntity<?> payLoan(String id, String amount);
+
+    ResponseEntity<?> payInterest(Long id);
+
+    Long calculateLoan();
+
+    Loan createLoan();
+
+    void generateInterest();
 }
